d9c751d607a30aa93ce71265c0d8b02f849ca0e3
Shanmugabavan <shanmugabavan25621@gmail.com>
2023-03-19 09:49:07 +0530

    bug fix

--- source/ronin_resnet.py
+++ source/ronin_resnet.py
@@ -100,7 +100,7 @@ def featTransformationModule(feat, device):
 
     # random_degrees = [random.uniform(0, math.pi/2) for j in range (feat.shape[0])]
     # random_degrees=[math.pi/90 for j in range (feat.shape[0])]
-    random_degrees=[math.pi/90,math.pi/80,math.pi/75,math.pi/70]*(feat.shape[0]/4)
+    random_degrees=[math.pi/90,math.pi/80,math.pi/75,math.pi/70]*int(feat.shape[0]/4)
     for i in range (feat.shape[0]):
         theta = random_degrees[i]  # angle of rotation in radians
         cos_theta = math.cos(theta)
